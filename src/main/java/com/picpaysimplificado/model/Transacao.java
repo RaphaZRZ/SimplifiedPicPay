3e1ad8cfe49236c80ae04a10b7f33e77802de1b8
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2024-11-26 21:51:16 -0300

    feat: create Transacao entity

---
+++ src/main/java/com/picpaysimplificado/model/Transacao.java
@@ -0,0 +1,35 @@
+package com.picpaysimplificado.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "Transacao")
+@Table(name = "Transacao")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id", unique = true)
+    private Long id;
+
+    @Column(name = "valor")
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "pagador_id")
+    private Usuario sender;
+
+    @ManyToOne
+    @JoinColumn(name = "recebedor_id")
+    private Usuario receiver;
+
+    @Column(name = "horario_transacao")
+    private LocalDateTime timestamp;
+}
