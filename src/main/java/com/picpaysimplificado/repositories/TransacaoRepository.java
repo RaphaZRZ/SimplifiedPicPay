f7985418c42be96ea0e29e3fa4452b85ec479cea
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2024-11-26 21:53:37 -0300

    feat: create Transacao repository

---
+++ src/main/java/com/picpaysimplificado/repositories/TransacaoRepository.java
@@ -0,0 +1,10 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.model.Transacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+
+}
