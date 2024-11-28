591b8b9eb61e6cdc682d217f63297b92c81d21d9
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2024-11-27 21:31:52 -0300

    feat: create TransactionController

---
+++ src/main/java/com/picpaysimplificado/controllers/TransactionController.java
@@ -0,0 +1,26 @@
+package com.picpaysimplificado.controllers;
+
+import com.picpaysimplificado.dtos.TransactionDTO;
+import com.picpaysimplificado.model.Transaction;
+import com.picpaysimplificado.services.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+    @Autowired
+    TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@Valid @RequestBody TransactionDTO transactionData) throws Exception {
+        Transaction transaction = this.transactionService.createTransaction(transactionData);
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+}
