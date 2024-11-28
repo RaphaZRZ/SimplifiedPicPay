af4e61736f64404be17c828c81766a44e168babd
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2024-11-28 20:36:26 -0300

    style: rename configs to config

--- src/main/java/com/picpaysimplificado/config/AppConfig.java
+++ src/main/java/com/picpaysimplificado/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.picpaysimplificado.configs;
+package com.picpaysimplificado.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
