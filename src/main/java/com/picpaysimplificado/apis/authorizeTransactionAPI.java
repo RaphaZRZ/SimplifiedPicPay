8cc00e81b0e8fc3d1688931b407026b2a471d138
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2024-11-28 20:02:27 -0300

    feat: create authorizeTransactionAPI

---
+++ src/main/java/com/picpaysimplificado/apis/authorizeTransactionAPI.java
@@ -0,0 +1,18 @@
+package com.picpaysimplificado.apis;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authorize-transaction-api")
+public class authorizeTransactionAPI {
+    @GetMapping
+    public ResponseEntity<Boolean> authorize() {
+        // simula a lógica do consumo de uma API
+        boolean authorized = Math.random() > 0.3; // 70% de chance de retornar autorizado
+
+        return ResponseEntity.ok(authorized);
+    }
+}
