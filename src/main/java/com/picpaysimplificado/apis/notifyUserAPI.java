c18b4f4890032e784ee47bcf472a9f1e260214e0
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2024-12-02 20:02:20 -0300

    feat: create notifyUserAPI

---
+++ src/main/java/com/picpaysimplificado/apis/notifyUserAPI.java
@@ -0,0 +1,18 @@
+package com.picpaysimplificado.apis;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notify-user-api")
+public class notifyUserAPI {
+    @GetMapping
+    public ResponseEntity<Boolean> notificationSended() {
+        // simula a lógica do consumo de uma API
+        boolean isSended = Math.random() > 0.2; // 80% de chance de ser notificado
+
+        return ResponseEntity.ok(isSended);
+    }
+}
