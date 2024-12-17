a138a9d1157464a3ff7a615543310ea4aac1373c
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2024-12-17 20:09:09 -0300

    feat: create handleControllerNotFoundException and handleTypeMismatch

--- src/main/java/com/picpaysimplificado/configs/ControllerExceptionHandler.java
+++ src/main/java/com/picpaysimplificado/configs/ControllerExceptionHandler.java
@@ -10,10 +10,13 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice // Inicializa a classe na inicialização do Spring Boot
 public class ControllerExceptionHandler {
-    @ExceptionHandler(DataIntegrityViolationException.class) // Validar se o usuário já está cadastrado
+    // Validar se o usuário já está cadastrado
+    @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity handleDuplicateEntry(DataIntegrityViolationException exception) {
         ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já cadastrado.", "400");
         return ResponseEntity.badRequest().body(exceptionDTO);
@@ -41,9 +44,24 @@ public class ControllerExceptionHandler {
         return ResponseEntity.badRequest().body(exceptionDTO);
     }
 
+    // Verifica se o tipo do parâmetro passados na URL da requisição corresponde ao esperado
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionDTO> handleTypeMismatch(MethodArgumentTypeMismatchException exception) {
+        String message = String.format("Entrada inválida: O parâmetro '%s' fornecido é de um tipo inválido.", exception.getName());
+        ExceptionDTO exceptionDTO = new ExceptionDTO(message, "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity handle404(EntityNotFoundException exception) {
         ExceptionDTO exceptionDTO = new ExceptionDTO("Recurso não encontrado.", "404");
+        return ResponseEntity.status(404).body(exceptionDTO);
+    }
+
+    // Verifica se há um controlador para a URL solicitada
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity handleControllerNotFoundException(NoHandlerFoundException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Controlador inexistente para esta URL.", "404");
         return ResponseEntity.status(404).body(exceptionDTO);
     }
 
