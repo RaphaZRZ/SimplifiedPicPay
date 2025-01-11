04b163ded93d65df89079fee5d682811e04efb9b
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2025-01-11 16:24:05 -0300

    refactor: rename the method findUserByDocument to findByDocument

--- src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
+++ src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -33,21 +33,21 @@ class UserRepositoryTest {
 
     @Test
     @DisplayName("Must get User Successfully from DB.")
-    void findUserByDocumentSuccess() throws Exception {
+    void findByDocumentSuccess() throws Exception {
         String document = "75292019735";
         UserDTO userData = new UserDTO("Magnus", "Petrikov", document, "magnus@gmail.com",
                 "magnusSenha", new BigDecimal("50.00"), UserType.COMMON);
         this.saveUser(userData);
 
-        Optional<User> result = this.userRepository.findUserByDocument(document);
+        Optional<User> result = this.userRepository.findByDocument(document);
 
         assertThat(result.isPresent()).isTrue();
     }
 
     @Test
     @DisplayName("Must not get User from DB when user not exists.")
-    void findUserByDocumentFailedDocumentNotFound() {
-        Optional<User> result = this.userRepository.findUserByDocument("99999999999");
+    void findByDocumentFailedDocumentNotFound() {
+        Optional<User> result = this.userRepository.findByDocument("99999999999");
         assertThat(result.isEmpty()).isTrue();
     }
 }
